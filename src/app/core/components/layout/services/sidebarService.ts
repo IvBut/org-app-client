63ae1b1a4def73fb94ad7ab6ae74271685073587
Бутько Иван Александрович (4049395) <ivanbutko5@gmail.com>
2024-08-27 11:58:29 +0300

    (feat) Добавление расширяемой боковой панели

--- src/app/core/components/layout/services/sidebarService.ts
+++ src/app/core/components/layout/services/sidebarService.ts
@@ -4,17 +4,33 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 import { SidenavContentAreaDirective } from '../directive/sidenav-content-area.directive';
 
+type TResizingEvent = {
+  isResizing: boolean;
+  startingCursorX: number;
+  startingWidth: number;
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class SidebarService {
+  public readonly MIN_WIDTH = 60;
+  public readonly MAX_WIDTH = 280;
   private sidenavContent: SidenavContentAreaDirective;
 
   private componentsStack: Component<unknown>[] = [];
 
   private isOpened = new BehaviorSubject<boolean>(false);
+  private resizingEvent = new BehaviorSubject<TResizingEvent>({
+    isResizing: false,
+    startingCursorX: 0,
+    startingWidth: 0
+  });
+  private width = new BehaviorSubject<number>(this.MIN_WIDTH);
 
   private isOpened$ = this.isOpened.asObservable();
+  private resizingEvent$ = this.resizingEvent.asObservable();
+  private width$ = this.width.asObservable();
 
   public set isOpen(val: boolean) {
     this.isOpened.next(val);
@@ -22,6 +38,27 @@ export class SidebarService {
 
   public get isOpen(): Observable<boolean> {
     return this.isOpened$;
+  }
+
+  public get resizing() {
+    return this.resizingEvent$;
+  }
+
+  public getResizingEvent() {
+    return this.resizingEvent.getValue();
+  }
+
+  public setResizingEvent(data: Partial<TResizingEvent>) {
+    this.resizingEvent.next({ ...this.resizingEvent.getValue(), ...data } as TResizingEvent);
+  }
+
+  public get sideBarWidth(): Observable<number> {
+    return this.width$;
+  }
+
+  public set sideBarWidth(width: number) {
+    const clampedWidth = Math.min(Math.max(width, this.MIN_WIDTH), this.MAX_WIDTH);
+    this.width.next(clampedWidth);
   }
 
   private attach(component: Component<unknown>) {
--- src/app/core/components/layout/sidebar/sidebar.component.ts
+++ src/app/core/components/layout/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  HostListener,
   inject,
   ViewChild
 } from '@angular/core';
@@ -28,6 +29,41 @@ export class SidebarComponent implements AfterViewInit {
   }
 
   toggle(value: boolean) {
+    const newWidth = value ? this.sidebarService.MAX_WIDTH : this.sidebarService.MIN_WIDTH;
     this.sidebarService.isOpen = value;
+    this.sidebarService.sideBarWidth = newWidth;
+    this.sidebarService.setResizingEvent({
+      isResizing: false,
+      startingWidth: newWidth,
+      startingCursorX: 0
+    });
+  }
+
+  startResizing(event: MouseEvent, startingWidth: number): void {
+    this.sidebarService.setResizingEvent({
+      isResizing: true,
+      startingCursorX: event.clientX,
+      startingWidth
+    });
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  updateSidenavWidth(event: MouseEvent) {
+    const resizingEvent = this.sidebarService.getResizingEvent();
+    if (!resizingEvent.isResizing) {
+      return;
+    }
+    const cursorDeltaX = event.clientX - resizingEvent.startingCursorX;
+    const newWidth = resizingEvent.startingWidth + cursorDeltaX;
+
+    this.sidebarService.isOpen = newWidth > this.sidebarService.MIN_WIDTH;
+    this.sidebarService.sideBarWidth = resizingEvent.startingWidth + cursorDeltaX;
+  }
+
+  @HostListener('window:mouseup')
+  stopResizing() {
+    this.sidebarService.setResizingEvent({
+      isResizing: false
+    });
   }
 }
